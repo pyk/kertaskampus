39dca2c56b9b0a2d860d1ad006aa0840a6bd31d7
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2015-11-17 08:05:48 +0700

    integrate with firebase

--- config/environment.js
+++ config/environment.js
@@ -4,6 +4,8 @@ module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'kertaskampus',
     environment: environment,
+    contentSecurityPolicy: { 'connect-src': "'self' https://auth.firebase.com wss://*.firebaseio.com" },
+    firebase: 'https://kertaskampus.firebaseio.com/',
     baseURL: '/',
     locationType: 'auto',
     EmberENV: {
